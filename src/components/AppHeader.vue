55e76e8edda77485147316177906061eaeaf50f7
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-09-23 20:33:24 +0200

    types added

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,7 +9,7 @@ export default {
         <nav class="navbar bg-body-secondary">
             <div class="container-fluid d-flex justify-content-center">
                 <a class="navbar-brand mx-5" href="#">Pokèdex</a>
-                <img src="../../public/pokeball-logo.png" alt="Logo">
+                <img src="../assets/pokeball-logo.png" alt="Logo">
                 <a class="navbar-brand mx-5" href="#">Contatti</a>
             </div>
         </nav>
--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -27,14 +27,21 @@ export default {
 <template>
     <div v-for="pokemon in this.store.allPokemon" class="card m-3 d-inline-block  text-center" style="width: 18rem;">
         <div class="card-body">
-            <div class="text-secondary fs-6">#{{ pokemon.id }}</div>
+            <div class="text-secondary fs-5 pb-3">#{{ pokemon.id }}</div>
             <img :src="'../../public/images/' + this.addZeroNumber(pokemon.id) + '.png'" :alt="pokemon.name.english"
                 class="w-100">
-            <h5 class="card-title py-2">{{ pokemon.name.english }}
+            <h5 class="card-title pt-2">{{ pokemon.name.english }}
             </h5>
-            <a href="#" class="btn btn-primary">Vedi dettagli</a>
+            <div class="types">
+                <span v-for="tipo in pokemon.type" class="type d-inline-block mb-3 mx-1 rounded px-2">{{ tipo }}</span>
+            </div>
+            <a href="#" class="btn btn-primary d-block">Vedi dettagli</a>
         </div>
     </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.type {
+    border: 2px solid black;
+}
+</style>
